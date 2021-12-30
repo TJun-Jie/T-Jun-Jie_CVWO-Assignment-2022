c40d8f4153b72dcc74b40ec69dc2ee6616884e98
TJun-Jie <tjunjie2000@gmail.com>
2021-12-30 11:23:56 +0800

    feat: add environment variables support

--- backend/cmd/api/helper/helper.go
+++ backend/cmd/api/helper/helper.go
@@ -8,12 +8,44 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/spf13/viper"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+func viperEnvVariable(key string) string {
+
+	// SetConfigFile explicitly defines the path, name and extension of the config file.
+	// Viper will use this and not check any of the config paths.
+	// .env - It will search for the .env file in the current directory
+	viper.SetConfigFile(".env")
+  
+	// Find and read the config file
+	err := viper.ReadInConfig()
+  
+	if err != nil {
+	  log.Fatalf("Error while reading config file %s", err)
+	}
+  
+	// viper.Get() returns an empty interface{}
+	// to get the underlying type of the key,
+	// we have to do the type assertion, we know the underlying value is string
+	// if we type assert to other type it will throw an error
+	value, ok := viper.Get(key).(string)
+  
+	// If the type is a string then ok will be true
+	// ok will make sure the program not break
+	if !ok {
+	  log.Fatalf("Invalid type assertion")
+	}
+  
+	return value
+  }
+
 func ConnectDB() *mongo.Client {
-	clientOptions := options.Client().ApplyURI("mongodb+srv://jjtai:zMxPRmkThZRqCVmc@cvwo-to-do.9wfav.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+	viperenv := viperEnvVariable("MONGO_DB_URI")
+	
+	clientOptions := options.Client().ApplyURI(viperenv)
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
