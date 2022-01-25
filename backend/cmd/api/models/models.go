d2ab94f94307f45c332672eac6e5747fda293ba2
JunJie <tjunjie2000@gmail.com>
2022-01-25 19:19:59 +0800

    fix: refactor backend

--- backend/cmd/api/models/models.go
+++ backend/cmd/api/models/models.go
@@ -3,15 +3,14 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Task struct {
-	ID     primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Title  string             `json:"title" bson:"title,omitempty"`
-	Description  string       `json:"description" bson:"description,omitempty"`
-	Completed  bool       `json:"completed" bson:"completed"`
-	PriorityID int  `json:"priorityID" bson:"priorityID,omitempty"`
+	ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Title       string             `json:"title" bson:"title,omitempty"`
+	Description string             `json:"description" bson:"description,omitempty"`
+	Completed   bool               `json:"completed" bson:"completed"`
+	PriorityID  int                `json:"priorityID" bson:"priorityID,omitempty"`
 }
 
 type Priority struct {
-	ID     int `json:"_id,omitempty" bson:"_id,omitempty"`
-	Description string  `json:"description" bson:"description,omitempty"`
+	ID          int    `json:"_id,omitempty" bson:"_id,omitempty"`
+	Description string `json:"description" bson:"description,omitempty"`
 }
-
